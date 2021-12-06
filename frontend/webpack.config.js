433a6136183ab8752f7d2f1e70b279dab16dc8ee
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-06 15:10:05 -0600

    init frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, "./src/index.js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        resolve: { extensions: [".js", ".jsx"] },
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./public/index.ejs",
+      filename: "./index.html",
+      title: "Open Trade Journal"
+    })
+  ]
+};
