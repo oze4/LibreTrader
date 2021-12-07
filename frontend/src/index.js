7c47158518947130cea6f25c2c2f714fac91da5a
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-07 11:25:29 -0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { CssBaseline } from '@mui/material';
 import { StyledEngineProvider } from '@mui/material/styles';
-
 import App from "./App";
 import { ColorModeProvider } from './theme'
 
