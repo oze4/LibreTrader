4bfac53e64a7ef63eb592413260c721c9933376e
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-06 20:52:38 -0600

    chore: to optimize or not to optimize
    - yes

--- frontend/src/theme/provider.js
+++ frontend/src/theme/provider.js
@@ -2,33 +2,30 @@ import React, { useState, useMemo } from "react";
 import { createTheme, ThemeProvider } from "@mui/material";
 
 import { darkMode, lightMode } from "./themes.js";
-import ColorModeContext from './context';
+import ColorModeContext from "./context";
 
 export default function ColorModeProvider({ children }) {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState(lightMode.palette.type);
+
   const colorMode = useMemo(
     () => ({
       toggle: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
       },
-      mode
+      mode,
     }),
-    [mode],
+    [mode]
   );
 
-  const theme = useMemo(
-    () => {
-      if (mode === "dark") return createTheme(darkMode);
-      return createTheme(lightMode);
-    },
-    [mode],
-  );
+  const theme = useMemo(() => generateTheme(mode), [mode]);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        {children}
-      </ThemeProvider>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </ColorModeContext.Provider>
   );
+}
+
+function generateTheme(mode) {
+  return mode === "dark" ? createTheme(darkMode) : createTheme(lightMode);
 }
