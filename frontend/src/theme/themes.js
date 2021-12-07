664dc2f6e3c46b62540a5f0644afc477cf232f0a
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-07 16:51:26 -0600

    Update themes.js

--- frontend/src/theme/themes.js
+++ frontend/src/theme/themes.js
@@ -8,65 +8,22 @@ export const lightMode = {
   palette: {
     type: "light",
     primary: {
-      main: "#779ecb",
+      main: "#1976d2",
     },
     secondary: {
-      main: "#9e465f",
-      light: "#e36588",
+      main: "rgb(220, 0, 78)",
     },
     background: {
-      default: "#faf8f6",
-      paper: "#f5f5f5",
-    },
-    text: {
-      primary: "rgba(0,0,0,0.87)",
+      default: "#fff",
+      paper: "#fff",
     },
   },
   typography: {
     fontFamily: "Roboto Condensed",
   },
-  overrides: {
-    MuiSwitch: {
-      root: {
-        width: 42,
-        height: 26,
-        padding: 0,
-        margin: 8,
-      },
-      switchBase: {
-        padding: 1,
-        "&$checked, &$colorPrimary$checked, &$colorSecondary$checked": {
-          transform: "translateX(16px)",
-          color: "#fff",
-          "& + $track": {
-            opacity: 1,
-            border: "none",
-          },
-        },
-      },
-      thumb: {
-        width: 24,
-        height: 24,
-      },
-      track: {
-        borderRadius: 13,
-        border: "1px solid #bdbdbd",
-        backgroundColor: "#fafafa",
-        opacity: 1,
-        transition:
-          "background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-      },
-    },
-  },
   props: {
-    MuiList: {
-      dense: true,
-    },
-    MuiMenuItem: {
-      dense: true,
-    },
-    MuiTable: {
-      size: "small",
+    MuiTooltip: {
+      arrow: true,
     },
     MuiButton: {
       size: "small",
@@ -104,6 +61,15 @@ export const lightMode = {
     },
     MuiTextField: {
       margin: "dense",
+      size: "small",
+    },
+    MuiList: {
+      dense: true,
+    },
+    MuiMenuItem: {
+      dense: true,
+    },
+    MuiTable: {
       size: "small",
     },
   },
@@ -111,59 +77,60 @@ export const lightMode = {
 
 export const darkMode = {
   palette: {
-    type: "dark",
+    type: 'dark',
     primary: {
-      main: "#434343",
+      main: '#90caf9',
     },
     secondary: {
-      main: "#9e465f",
-      light: "#e36588",
+      main: '#f48fb1',
     },
     background: {
-      default: "#141414",
-      paper: "#1b1b1b",
-    },
-    text: {
-      primary: "rgba(255,255,255,0.87)",
-    },
-  },
-  typography: {
-    fontFamily: "Roboto Condensed",
-  },
-  overrides: {
-    MuiSwitch: {
-      root: {
-        width: 42,
-        height: 26,
-        padding: 0,
-        margin: 8,
-      },
-      switchBase: {
-        padding: 1,
-        "&$checked, &$colorPrimary$checked, &$colorSecondary$checked": {
-          transform: "translateX(16px)",
-          color: "#fff",
-          "& + $track": {
-            opacity: 1,
-            border: "none",
-          },
-        },
-      },
-      thumb: {
-        width: 24,
-        height: 24,
-      },
-      track: {
-        borderRadius: 13,
-        border: "1px solid #bdbdbd",
-        backgroundColor: "#fafafa",
-        opacity: 1,
-        transition:
-          "background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-      },
+      default: '#212121',
+      paper: '#424242',
     },
   },
   props: {
+    MuiTooltip: {
+      arrow: true,
+    },
+    MuiButton: {
+      size: 'small',
+    },
+    MuiButtonGroup: {
+      size: 'small',
+    },
+    MuiCheckbox: {
+      size: 'small',
+    },
+    MuiFab: {
+      size: 'small',
+    },
+    MuiFormControl: {
+      margin: 'dense',
+      size: 'small',
+    },
+    MuiFormHelperText: {
+      margin: 'dense',
+    },
+    MuiIconButton: {
+      size: 'small',
+    },
+    MuiInputBase: {
+      margin: 'dense',
+    },
+    MuiInputLabel: {
+      margin: 'dense',
+    },
+    MuiRadio: {
+      size: 'small',
+    },
+    MuiSwitch: {
+      size: 'small',
+    },
+    MuiTextField: {
+      margin: 'dense',
+      size: 'small',
+    },
     MuiList: {
       dense: true,
     },
@@ -171,45 +138,10 @@ export const darkMode = {
       dense: true,
     },
     MuiTable: {
-      size: "small",
+      size: 'small',
     },
-    MuiButton: {
-      size: "small",
-    },
-    MuiButtonGroup: {
-      size: "small",
-    },
-    MuiCheckbox: {
-      size: "small",
-    },
-    MuiFab: {
-      size: "small",
-    },
-    MuiFormControl: {
-      margin: "dense",
-      size: "small",
-    },
-    MuiFormHelperText: {
-      margin: "dense",
-    },
-    MuiIconButton: {
-      size: "small",
-    },
-    MuiInputBase: {
-      margin: "dense",
-    },
-    MuiInputLabel: {
-      margin: "dense",
-    },
-    MuiRadio: {
-      size: "small",
-    },
-    MuiSwitch: {
-      size: "small",
-    },
-    MuiTextField: {
-      margin: "dense",
-      size: "small",
-    },
+  },
+  typography: {
+    fontFamily: 'Roboto Condensed',
   },
 };
