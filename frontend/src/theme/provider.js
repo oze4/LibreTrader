3ecaaf048668ce12b812c36e298c5bf7d6ecec59
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-06 21:06:04 -0600

    Update provider.js

--- frontend/src/theme/provider.js
+++ frontend/src/theme/provider.js
@@ -4,14 +4,18 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import { darkMode, lightMode } from "./themes.js";
 import ColorModeContext from "./context";
 
+/**
+ * @notes
+ *  - ColorModeProvider is React context that wraps our app which allows us to easily toggle between dark and light modes.
+ *  - 
+ */
+
 export default function ColorModeProvider({ children }) {
   const [mode, setMode] = useState(lightMode.palette.type);
 
   const colorMode = useMemo(
     () => ({
-      toggle: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-      },
+      toggle: () => setMode(handleSetMode),
       mode,
     }),
     [mode]
@@ -26,6 +30,20 @@ export default function ColorModeProvider({ children }) {
   );
 }
 
+//// INTERNAL FUNCTIONS 
+
+/**
+ * @function handleSetMode state handler for color mode
+ * @param {String} previousMode 
+ */
+function handleSetMode(previousMode) {
+  return previousMode === "dark" ? "light" : "dark"
+}
+
+/**
+ * @function generateTheme wraps mui createTheme call in order to determine color mode.
+ * @param {String} mode ("dark"|"light")
+ */
 function generateTheme(mode) {
   return mode === "dark" ? createTheme(darkMode) : createTheme(lightMode);
 }
