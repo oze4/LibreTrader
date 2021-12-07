893f3f8e0ad78f3e3c7672d51735b59007650f6c
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-06 20:54:22 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Button, Typography, Paper } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 import { ColorModeContext } from "./theme";
 
@@ -13,9 +13,6 @@ export default function App() {
       <Topbar />
       <Container fluid>
         <Typography variant="h1">Hello, App!</Typography>
-        <Button variant="contained" onClick={theme.toggle}>
-          Toggle Theme
-        </Button>
       </Container>
     </>
   );
