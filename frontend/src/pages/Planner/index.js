542996f3c5f1ea1fa451a76d5e03159a2fb5902d
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-07 20:25:56 -0600

    chore: see description
    - Define Trade Plan schema
    - More work on integrating Planner page

--- frontend/src/pages/Planner/index.js
+++ frontend/src/pages/Planner/index.js
@@ -1,6 +1,16 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Typography, Grid, Paper } from "@mui/material";
 
 export default function Planner() {
-  return <Typography variant="h1">I am the planner/planning page</Typography>;
+  const [date, setDate] = useState(undefined);
+  return (
+    <Grid container>
+      <Typography variant="h1" textAlign="center">
+        Trade Plan {date ? date : null}
+      </Typography>
+      <Grid item xs={12} marginTop={5}>
+        <Paper sx={{ minHeight: "70vh" }}>Helllllo</Paper>
+      </Grid>
+    </Grid>
+  )
 }
