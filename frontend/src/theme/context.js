d14276adf5381823cf4334f91e9c319e94ea798a
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-07 20:31:18 -0600

    chore: improve theme cookie check

--- frontend/src/theme/context.js
+++ frontend/src/theme/context.js
@@ -2,9 +2,11 @@ import { createContext } from "react";
 import Cookies from '@/utils/cookies';
 
 function getColorModeFromCookies() {
-  if (!Cookies.get("colorMode")) {
+  // If a cookie does not exist, set the default "colorMode" cookie to "light".
+  if (!Cookies.check("colorMode")) {
     Cookies.set("colorMode", "light", 720);
   }
+  // We want the cookie to be our single source of truth from the beginning. 
   return Cookies.get("colorMode");
 }
 
