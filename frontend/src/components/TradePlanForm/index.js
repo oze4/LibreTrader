bce81e9b1e8b5448f8d715a8ecb465fc2fb17d36
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-12 14:39:46 -0600

    feat(frontend): improve grid

--- frontend/src/components/TradePlanForm/index.js
+++ frontend/src/components/TradePlanForm/index.js
@@ -115,74 +115,76 @@ export default function TradePlan({ onSubmit }) {
             <Typography variant="subtitle1">Thesis</Typography>
             <Typography variant="subtitle2">overall market context and conditions</Typography>
           </Grid>
-          {/* bigger picture */}
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              multiline
-              rows={3}
-              onChange={(e) => handleTextFieldChange(e, "biggerPicture")}
-              label="Summary"
-              placeholder="market context &amp; conditions/bigger picture"
-            />
-          </Grid>
-          {/* add news or catalyst */}
-          <Grid item xs={12} md={6}>
-            <TextField
-              fullWidth
-              multiline
-              value={newsCatalyst}
-              onChange={(e) => setNewsCatalyst(e.target.value)}
-              label="News or Catalyst"
-              placeholder="add news or catalyst, if any"
-              InputProps={{
-                endAdornment: (
-                  <Tooltip title="add news/catalyst">
-                    <IconButton color="primary" onClick={handleAddNewsOrCatalyst}>
-                      <AddIcon />
-                    </IconButton>
-                  </Tooltip>
-                ),
-              }}
-            />
-          </Grid>
-          {/* display news and catalysts */}
-          <Grid item xs={12} md={6}>
-            <Paper
-              elevation={0}
-              ref={newsAndCatalystsListRef}
-              sx={{ minHeight: "130px", maxHeight: "200px", overflow: "auto" }}
-            >
-              <List
-                subheader={
-                  <ListSubheader sx={{ textAlign: "center" }}>News &amp; Catalysts</ListSubheader>
-                }
-              >
-                <Divider />
-                {state.newsAndCatalysts.map((news, index) => {
-                  return (
-                    <Fragment key={"" + news.length + index}>
-                      <Divider />
-                      <ListItem
-                        secondaryAction={
-                          <Tooltip arrow title="remove">
-                            <IconButton
-                              color="primary"
-                              edge="end"
-                              onClick={() => handleRemoveNews(index)}
-                            >
-                              <DeleteIcon />
-                            </IconButton>
-                          </Tooltip>
-                        }
-                      >
-                        <ListItemText primary={news} />
-                      </ListItem>
-                    </Fragment>
-                  );
-                })}
-              </List>
-            </Paper>
+          <Grid item xs={12} container spacing={2}>
+            <Grid item xs={12} md={6} container spacing={2}>
+              {/* bigger picture */}
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  multiline
+                  rows={3}
+                  onChange={(e) => handleTextFieldChange(e, "biggerPicture")}
+                  label="Summary"
+                  placeholder="market context &amp; conditions/bigger picture"
+                />
+              </Grid>
+              {/* add news or catalyst */}
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  multiline
+                  value={newsCatalyst}
+                  onChange={(e) => setNewsCatalyst(e.target.value)}
+                  label="News or Catalyst"
+                  placeholder="add news or catalyst, if any"
+                  InputProps={{
+                    endAdornment: (
+                      <Tooltip title="add news/catalyst">
+                        <IconButton color="primary" onClick={handleAddNewsOrCatalyst}>
+                          <AddIcon />
+                        </IconButton>
+                      </Tooltip>
+                    ),
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Grid item xs={12} md={6} container spacing={2}>
+              {/* display news and catalysts */}
+              <Grid item xs={12}>
+                <List
+                  ref={newsAndCatalystsListRef}
+                  sx={{ maxHeight: "180px", overflow: "scroll" }}
+                  subheader={
+                    <ListSubheader sx={{ textAlign: "center" }}>News &amp; Catalysts</ListSubheader>
+                  }
+                >
+                  <Divider />
+                  {state.newsAndCatalysts.map((news, index) => {
+                    return (
+                      <Fragment key={"" + news.length + index}>
+                        <Divider />
+                        <ListItem
+                          secondaryAction={
+                            <Tooltip arrow title="remove">
+                              <IconButton
+                                color="primary"
+                                edge="end"
+                                onClick={() => handleRemoveNews(index)}
+                              >
+                                <DeleteIcon />
+                              </IconButton>
+                            </Tooltip>
+                          }
+                        >
+                          <ListItemText primary={news} />
+                        </ListItem>
+                      </Fragment>
+                    );
+                  })}
+                </List>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Section>
