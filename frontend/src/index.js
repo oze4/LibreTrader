ea9c04489486d70c4ecd2949be0e9e2f4b9a97a8
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-18 22:13:52 -0600

    fix(frontend): switch to HashRouter bc of GH pages and how they handle SPA's

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import { StyledEngineProvider } from "@mui/material/styles";
 
@@ -10,10 +10,10 @@ import { ColorModeProvider } from "@/theme";
 ReactDOM.render(
   <StyledEngineProvider injectFirst>
     <ColorModeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <CssBaseline />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ColorModeProvider>
   </StyledEngineProvider>,
   document.querySelector("#root"),
