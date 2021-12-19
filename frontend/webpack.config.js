97706a8e98d7d1f9d8ae67947e94cb3c0a7d69e3
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2021-12-18 20:35:53 -0600

    build(frontend): so we can publish via GH pages

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     bundle: path.resolve(__dirname, "./src/index.js"),
   },
   output: {
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, "../docs"),
     filename: "libre-trader-fe-bundle.js",
     clean: true,
   },
